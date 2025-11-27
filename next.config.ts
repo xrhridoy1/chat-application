218dddd228114f2d9ceaab76f0473e8bed74a56b
xrhridoy1 <hmd740004@gmail.com>
2025-11-27 11:28:20 +0600

    last

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: 'https://gyhxweugupovdqyadnoq.supabase.co/auth/v1/:path*',
+      },
+      {
+        source: '/api/rest/:path*',
+        destination: 'https://gyhxweugupovdqyadnoq.supabase.co/rest/v1/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
