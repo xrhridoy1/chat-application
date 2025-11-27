7dbd7f344fc8bcff0086cdee229fa9f2c2ec5ad7
xrhridoy1 <hmd740004@gmail.com>
2025-11-27 11:19:23 +0600

    secode 5

---
+++ app/auth/page.tsx
@@ -0,0 +1,12 @@
+import AuthForm from '@/components/AuthForm/AuthForm';
+import React from 'react';
+
+const AuthPage = () => {
+    return (
+        <div >
+            <AuthForm />
+        </div>
+    );
+};
+
+export default AuthPage;
