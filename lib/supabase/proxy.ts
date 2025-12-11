46ff9f430b0c1184db42ad3ce0b69a3654682b9a
xrhridoy1 <hmd740004@gmail.com>
2025-12-11 09:38:32 +0600

    may be done 4

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -1,5 +1,7 @@
 import { createServerClient } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server';
+
+
 
 export async function updateSession(request: NextRequest) {
     let supabaseResponse = NextResponse.next({
