e8f45c928f2e51f1197eeec114f3da9d375fde46
xrhridoy1 <hmd740004@gmail.com>
2025-12-11 09:18:06 +0600

    may be done

--- components/AuthForm/AuthForm.tsx
+++ components/AuthForm/AuthForm.tsx
@@ -1,29 +1,13 @@
-'use client'
-
 import { signInWithGoogle } from '@/utils/action';
 import React from 'react';
-import { useFormStatus } from 'react-dom';
-import { Spinner } from '../ui/spinner';
+import Button from './Button';
 
-function SubmitButton() {
-    const { pending } = useFormStatus();
 
-    return (
-        <button
-            className="py-3 px-7 bg-slate-800 rounded text-white font-bold cursor-pointer"
-            disabled={pending}
-        >
-            {pending ? <div className='flex gap-3 items-center'>
-                <Spinner /> <span>Sign in With Google</span>
-            </div> : 'Sign in With Google'}
-        </button>
-    );
-}
 
 const AuthForm = () => {
     return (
         <form action={signInWithGoogle}>
-            <SubmitButton />
+            <Button />
         </form>
     );
 };
